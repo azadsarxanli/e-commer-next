6617eeb70f0b1ff1e5fabf4d887086c81307fff3
azadsarxanli <azad.sarxanli@gmail.com>
2022-07-03 19:52:26 +0400

    Navbar section done

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Link from "next/link";
 import { AiOutlineShopping } from "react-icons/ai";
+import { Cart } from "./";
+import { useStateContext } from "../context/StateContext";
 
 const NavBar = () => {
+  const { showCart, setShowCart, totalQuantity } = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
         <Link href={`/`}>JSM Headphones</Link>
       </p>
-      <button className="cart-icon" onClick={null} type="button">
+      <button
+        className="cart-icon"
+        onClick={() => setShowCart(true)}
+        type="button"
+      >
         <AiOutlineShopping />
-        <span className="cart-item-qty">0</span>
+        <span className="cart-item-qty">{totalQuantity}</span>
       </button>
+      {showCart && <Cart />}
     </div>
   );
 };
